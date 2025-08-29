6b14684c5eb8b3394fdbad14d6bb65458832a9e5
deciofjr <deciojr@jtpsolution.com.br>
2025-08-29 00:32:17 -0300

    Subindo correcao 2908

--- src/main/java/com/mercado/sistema/general/Constants.java
+++ src/main/java/com/mercado/sistema/general/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int TCTABELAPRECO = 3;
     public static final int TCIMAGEM = 1;
     public static final int TCVIDEO = 2;
+    public static final int TCOFERTA = 4;
 }
--- src/main/java/com/mercado/sistema/service/ConteudoTabelaPrecosService.java
+++ src/main/java/com/mercado/sistema/service/ConteudoTabelaPrecosService.java
@@ -2,6 +2,7 @@ package com.mercado.sistema.service;
 
 import com.mercado.sistema.dao.model.Conteudo;
 import com.mercado.sistema.dao.model.ConteudoTabelaPreco;
+import com.mercado.sistema.dao.model.Produto;
 import com.mercado.sistema.dao.repository.ConteudoTabelaPrecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,9 @@ public class ConteudoTabelaPrecosService {
 
     public List<ConteudoTabelaPreco> getConteudoTabelaPrecos(Conteudo conteudo) {
         return conteudoTabelaPrecoRepository.findByConteudo(conteudo);
+    }
 
+    public List<Produto> findProdutosConteudo(Long conteudoId) {
+        return conteudoTabelaPrecoRepository.findProdutosConteudo(conteudoId);
     }
 }
